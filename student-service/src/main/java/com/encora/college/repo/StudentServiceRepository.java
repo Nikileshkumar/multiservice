912c9497821098ae0116246560082d39d8d9ea0d
nikileshkumar <nikileshkumar333@gmail.com>
2023-02-05 13:44:40 +0530

    deleted src folder

--- student-service/src/main/java/com/encora/college/repo/StudentServiceRepository.java
+++ student-service/src/main/java/com/encora/college/repo/StudentServiceRepository.java
@@ -2,11 +2,12 @@ package com.encora.college.repo;
 
 import com.encora.college.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface StudentServiceRepository extends JpaRepository<Student, String> {
     List<Student> findByDepartmentId(String departmentId);
-
+    @Query("")
     Student findByStudentName(String studentName);
 }
